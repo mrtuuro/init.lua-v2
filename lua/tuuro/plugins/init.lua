8f98e7da1327ff116b9d3380a29e746a553329bf
mrtuuro <t.ozay98@gmail.com>
2024-11-14 21:11:46 +0300

    added java config

--- lua/tuuro/plugins/init.lua
+++ lua/tuuro/plugins/init.lua
@@ -19,49 +19,49 @@ return{
     },
 
     {
-	    'nvim-treesitter/nvim-treesitter',
-	    build = ':TSUpdate',  -- Automatically update parsers after installing
-	    config = function()
-		    require('nvim-treesitter.configs').setup({
-			    -- A list of parser names, or "all"
-			    ensure_installed = { 'go', 'c', 'lua' },
-			    -- Install languages synchronously (only applied to `ensure_installed`)
-			    sync_install = false,
-			    -- Automatically install missing parsers when entering buffer
-			    auto_install = true,
-			    highlight = {
-				    enable = true,    -- false will disable the whole extension
-				    additional_vim_regex_highlighting = false,
-			    },
-			    indent = {
-				    enable = true,
-			    },
-		    })
-	    end,
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',  -- Automatically update parsers after installing
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                -- A list of parser names, or "all"
+                ensure_installed = { 'go', 'c', 'lua', 'java' },
+                -- Install languages synchronously (only applied to `ensure_installed`)
+                sync_install = true,
+                -- Automatically install missing parsers when entering buffer
+                auto_install = true,
+                highlight = {
+                    enable = true,    -- false will disable the whole extension
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = true,
+                },
+            })
+        end,
     },
 
-	{
-		'mbbill/undotree',
-		config = function()
-			-- Set custom key mappings or other configurations if needed
-			vim.api.nvim_set_keymap('n', '<leader>u', ':UndotreeToggle<CR>', { noremap = true, silent = true })
-		end
-	},
+    {
+        'mbbill/undotree',
+        config = function()
+            -- Set custom key mappings or other configurations if needed
+            vim.api.nvim_set_keymap('n', '<leader>u', ':UndotreeToggle<CR>', { noremap = true, silent = true })
+        end
+    },
 
-	"tpope/vim-fugitive",
-	{
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		dependencies = { "nvim-lua/plenary.nvim" }
-	},
+    "tpope/vim-fugitive",
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
 
 
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
 
 
     {
@@ -101,6 +101,6 @@ return{
         -- use opts = {} for passing setup options
         -- this is equivalent to setup({}) function
     },
-    
+
 }
 
